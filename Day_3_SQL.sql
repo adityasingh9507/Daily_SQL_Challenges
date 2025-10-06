4bbda0b9b41217583ed859bf7e500551f3cdeef1
Aditya <adityakumarsingh99347@gmail.com>
2025-10-06 11:08:42 +0530

    Update Testing_SQL.sql

--- Day_3_SQL.sql
+++ Day_3_SQL.sql
@@ -29,4 +29,18 @@ from (select l.state,
     group by l.state
 ) as state_totals
 where Total_sales > avg_sales
+
 order by state;
+
+--GlobalMart Sales team would like to perform geographical analysis of sales across the 4 regions : East, South, West and Central.
+select l.region,l.state,sum(t.Sales_amount) as Sales,
+sum(sum(t.Sales_amount)) over(partition by l.region) as region_sales,
+sum(sum(t.sales_amount)) over() as Overall_Sales,
+sum(t.Sales_amount) * 100.0/ sum(sum(t.sales_amount)) over (partition by l.region) as Contribution_Percent,
+sum(t.Sales_amount) * 100.0/ sum(sum(t.sales_amount)) over () as Overall_Contribution
+from ex_locations l
+join ex_customers c on l.postal_code=c.postal_code
+join ex_orders o on c.customer_id=o.customer_id
+join ex_transactions t on o.order_id=t.Order_ID
+group by l.region,l.state
+order by l.region,l.state;
